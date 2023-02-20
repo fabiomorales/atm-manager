683e3deaf455f7f0bb54ec866199a09ad1b84428
fabio morales <fabio.l.morales@hotmail.com>
2023-02-20 10:10:04 -0300

    feature: implementa validação de requisição no modulo customer

---
+++ src/modules/customers/infra/controllers/CustomerControllerValidation.ts
@@ -0,0 +1,29 @@
+import { ICreateCustomer } from '@modules/customers/domain/models/ICreateCustomer';
+import { IFindCustomer } from '@modules/customers/domain/models/IFindCustomer';
+import { validation } from '@shared/infra/http/middlewares/ValidationRequest';
+import * as yup from 'yup';
+
+export const createValidation = validation(getSchema => ({
+  body: getSchema<ICreateCustomer>(
+    yup.object().shape({
+      fisrt_name: yup.string().required().min(2),
+      last_name: yup.string().required().min(2),
+      email: yup.string().required().email(),
+      cpf: yup.string().required().min(11),
+      balance: yup.number().required().min(2),
+    }),
+  ),
+  params: getSchema<IFindCustomer>(
+    yup.object().shape({
+      email: yup.string().required().email(),
+    }),
+  ),
+}));
+
+export const showValidation = validation(getSchema => ({
+  params: getSchema<IFindCustomer>(
+    yup.object().shape({
+      email: yup.string().required().email(),
+    }),
+  ),
+}));
--- src/modules/customers/infra/routes/customers.routes.ts
+++ src/modules/customers/infra/routes/customers.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
+import { createValidation, showValidation } from '../controllers/CustomerControllerValidation';
 import CustomersController from '../controllers/CustomersController';
 
 const customersRouter = Router();
 const customersController = new CustomersController();
 
-customersRouter.get('/:email', customersController.show);
+customersRouter.get('/:email', showValidation, customersController.show);
 
-customersRouter.post('/', customersController.create);
+customersRouter.post('/', createValidation, customersController.create);
 
 export default customersRouter;
